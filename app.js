61c89e6416fdcd66629e4906a726dbfceb613e43
UsmanKhanTaimure <107642292+UsmanKhanTaimure@users.noreply.github.com>
2022-06-16 10:52:52 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+//  Chapter 6 to 9;
+// Question 1
+// var a = 10;
+// document.write("The value is: " + a + "<br>")
+// ++a;
+// document.write("The value ++a: " + a + "<br>")
+
+// document.write("The Value is a++" + a +"<br>")
+// a++;
+// document.write("Now Value is " + a + "<br>")
+// --a;
+// document.write("The value  is --a " + a + "<br>")
+// document.write("Now value  is --a " + a)
+
+
+// Question 2;
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + --b;
+// document.write(result)
+
+
+// Question 3
+// var table = +prompt("Enter Your Table");
+// document.write(table + " x " + 1 + " = " +table*1 +"<br>")
+// document.write(table + " x " + 2 + " = " +table*2 +"<br>")
+// document.write(table + " x " + 3 + " = " +table*3 +"<br>")
+// document.write(table + " x " + 4 + " = " +table*4 +"<br>")
+// document.write(table + " x " + 5 + " = " +table*5 +"<br>")
+// document.write(table + " x " + 6 + " = " +table*6 +"<br>")
+// document.write(table + " x " + 7 + " = " +table*7 +"<br>")
+// document.write(table + " x " + 8 + " = " +table*8 +"<br>")
+// document.write(table + " x " + 9 + " = " +table*9 +"<br>")
+// document.write(table + " x " + 10 + " = " +table*10 +"<br>")
+
+
+// // Question 4
+// var sub1 = prompt("Enter Your Sub1 ");
+// var sub2 = prompt("Enter Your Sub2 ");
+// var sub3 = prompt("Enter Your Sub3 ");
+// var obt1 = +prompt("Enter your ob1");
+// var obt2 = +prompt("Enter your ob2");
+// var obt3 = +prompt("Enter your ob3");
+// var total = 100;
+// var perc = (obt1+obt2+obt3)/(total+total+total)*100;
+// document.write("<b>Subject</b> ")
+// document.write(" <b>Total</b> ")
+// document.write(" <b>obtained</b>" +"<br>")
+// document.write(sub1 +" ")
+// document.write(total +" ")
+// document.write(obt1 +"<br>")
+// document.write(sub2 +" ")
+// document.write(total +" ")
+// document.write(obt2 +"<br>")
+// document.write(sub3 +" ")
+// document.write(total +" ")
+// document.write(obt3 +" " + "<br>")
+// document.write("This is three Subject percente" +perc)
